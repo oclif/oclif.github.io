1047b36b28866630fa035ee80a4ebcf5db8843c5
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-03-22 15:33:39 -0700

    fix edit url

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -67,7 +67,7 @@ const siteConfig = {
   stylesheets: [
     'https://fonts.googleapis.com/css?family=Roboto|Roboto+Mono',
   ],
-  editUrl: 'https://github.com/oclif/oclif.github.io/tree/docs/docs',
+  editUrl: 'https://github.com/oclif/oclif.github.io/tree/docs/docs/',
 };
 
 module.exports = siteConfig;
