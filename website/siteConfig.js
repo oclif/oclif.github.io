ff224a46dd8f1e1916dd5258f93af4db7647a08d
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-03-23 17:43:37 -0700

    command -> commands

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -25,7 +25,7 @@ const siteConfig = {
   projectName: 'oclif.github.io',
   headerLinks: [
     {doc: 'introduction', label: 'Getting Started'},
-    {doc: 'command', label: 'API Reference'},
+    {doc: 'commands', label: 'API Reference'},
     {blog: true, label: 'Blog'},
     {href: 'https://gitter.im/oclif/oclif', label: 'Discuss'},
     {href: 'https://github.com/oclif/oclif', label: 'GitHub'},
