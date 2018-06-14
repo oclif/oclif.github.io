1f9850c96cb51a783b649ea645446dc1152950b8
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-06-14 11:12:23 -0700

    now hiring!

---
+++ docs/now_hiring.md
@@ -0,0 +1,5 @@
+---
+title: Now Hiring!
+---
+
+Like TypeScript? Want to work on oclif and the Heroku CLI full time? Send me (Jeff Dickey) a message in [gitter](https://gitter.im/oclif/oclif) and tell me a little about yourself.
--- website/siteConfig.js
+++ website/siteConfig.js
@@ -29,7 +29,7 @@ const siteConfig = {
     {blog: true, label: 'Blog'},
     {href: 'https://gitter.im/oclif/oclif', label: 'Discuss'},
     {href: 'https://github.com/oclif/oclif', label: 'GitHub'},
-    // {blog: true, label: 'Blog'},
+    {doc: 'now_hiring', label: 'Now Hiring!'},
   ],
   // users,
   /* path to images for header/footer */
